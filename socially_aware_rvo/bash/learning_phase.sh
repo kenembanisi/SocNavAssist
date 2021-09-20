0edc73d9ce729fb88f9bee410f589b26dc062405
Kenechukwu Mbanisi <kene.mbanisi@gmail.com>
2021-09-20 09:29:02 -0400

    upd bash scripts

---
+++ socially_aware_rvo/bash/learning_phase.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Launch the robot
+source /opt/ros/melodic/setup.bash
+source ~/workspaces/research_ws/devel/setup.bash
+
+echo "Starting the user study, kindly wait!"
+
+PARTICIPANT_ID="S04"
+
+PHASE="learning"
+
+########################################## learning phase #############################################
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="approach-01" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="MC" trial_number:="1/12" case6:="1"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-01" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="H" trial_number:="2/12" case4:="1"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="random-02" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="V-T" trial_number:="3/12" case1:="1"
+# 4
+roslaunch socially_aware_rvo trial.launch scenario:="random-01" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="HV-T" trial_number:="4/12" case4:="1"
+# 5
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-02" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="V-B" trial_number:="5/12" case6:="1"
+# 6
+roslaunch socially_aware_rvo trial.launch scenario:="approach-02" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="HV-B" trial_number:="6/12" case1:="1"
+# ------------------------------------------------------------------------------------------------------
+# 7
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-03" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="HV-T" trial_number:="7/12" case1:="1"
+# 8
+roslaunch socially_aware_rvo trial.launch scenario:="approach-01" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="H" trial_number:="8/12" case6:="1"
+# 9
+roslaunch socially_aware_rvo trial.launch scenario:="random-02" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="V-B" trial_number:="9/12" case1:="1"
+# 10
+roslaunch socially_aware_rvo trial.launch scenario:="approach-02" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="MC" trial_number:="10/12" case4:="1"
+# 11
+roslaunch socially_aware_rvo trial.launch scenario:="random-01" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="HV-B" trial_number:="11/12" case1:="1"
+# 12
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-01" trial_name:=$PARTICIPANT_ID phase:=$PHASE trial_condition:="V-T" trial_number:="12/12" case4:="1"
+
+
+# -----------------------------------------------------------------------------------------------------
+
+echo "
+
+ -------------------    Learning Phase Complete!    ---------------------
+
+ "
---
+++ socially_aware_rvo/bash/pilot_study/testing_phase_P02.sh
@@ -0,0 +1,156 @@
+#!/bin/sh
+
+# Launch the robot
+source /opt/ros/melodic/setup.bash
+source ~/workspaces/research_ws/devel/setup.bash
+
+echo "Starting the user study, kindly wait!"
+
+PARTICIPANT_ID="P02"
+
+PHASE="test"
+
+########################################## testing phase #############################################
+
+# block 1 ---------------------------------------------------------------------------------------------
+CONDITION="V-B"
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="approach-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="1/18" trial_case:="case1"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-03" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="2/18" trial_case:="case2"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="random-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="3/18" trial_case:="case2"
+
+# -----------------------------------------------------------------------------------------------------
+ 
+ echo "
+ -------------------Block 1 complete---------------------
+ "
+
+ sleep 1m # waits for 1 minute
+
+# block 2 ---------------------------------------------------------------------------------------------
+CONDITION="H"
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="4/18" trial_case:="case2"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="approach-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="5/18" trial_case:="case1"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="random-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="6/18" trial_case:="case2"
+# -----------------------------------------------------------------------------------------------------
+
+echo "
+ -------------------Block 2 complete---------------------
+ "
+
+ sleep 1m # waits for 1 minute
+
+# block 3 ---------------------------------------------------------------------------------------------
+CONDITION="HV-B"
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="random-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="7/18" trial_case:="case1"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-03" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="8/18" trial_case:="case2"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="approach-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="9/18" trial_case:="case2"
+# -----------------------------------------------------------------------------------------------------
+
+echo "
+ -------------------Block 3 complete---------------------
+ "
+sleep 1m # waits for 1 minute
+
+
+ # block 4 ---------------------------------------------------------------------------------------------
+CONDITION="MC"
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="10/18" trial_case:="case1"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="approach-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="11/18" trial_case:="case1"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="random-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="12/18" trial_case:="case2"
+# -----------------------------------------------------------------------------------------------------
+
+echo "
+ -------------------Block 4 complete---------------------
+ "
+
+sleep 1m # waits for 1 minute
+
+
+ # block 5 ---------------------------------------------------------------------------------------------
+CONDITION="V-T"
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="approach-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="13/18" trial_case:="case1"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-03" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="14/18" trial_case:="case2"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="random-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="15/18" trial_case:="case2"
+# -----------------------------------------------------------------------------------------------------
+
+echo "
+ -------------------Block 5 complete---------------------
+ "
+
+ sleep 1m # waits for 1 minute
+
+# block 6 ---------------------------------------------------------------------------------------------
+
+CONDITION="HV-T"
+
+# 1
+roslaunch socially_aware_rvo trial.launch scenario:="crossing-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="16/18" trial_case:="case1"
+# 2
+roslaunch socially_aware_rvo trial.launch scenario:="approach-02" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="17/18" trial_case:="case1"
+# 3
+roslaunch socially_aware_rvo trial.launch scenario:="random-01" trial_name:=$PARTICIPANT_ID \
+                                          phase:=$PHASE trial_condition:=$CONDITION \
+                                          trial_number:="18/18" trial_case:="case1"
+# -----------------------------------------------------------------------------------------------------
+
+echo "
+
+ -------------------Block 6 complete---------------------
+ 
+ -------------------User Study is Over!!!---------------------
+
+ "
